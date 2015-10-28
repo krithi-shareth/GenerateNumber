122b83cbbad4a50c073a03cadb1d346cd4cbd961
krithi-shareth <krithi.shareth@gmail.com>
2015-10-27 23:00:49 -0500

    Program
    Program added

---
+++ Test/ClassLibrary2/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintNumberLibrary
+{
+    public class PrintNumberClass
+    {
+        public static string[] printnumber(int n, params KeyValuePair<int, string>[] pairs)
+        {
+
+            string[] output = new string[n + 1];
+            for (int i = 1; i <= n; i++)
+            {
+                output[i] = i.ToString();
+                //Console.WriteLine(i);
+
+                foreach (var args in pairs)
+                {
+                    if (i % args.Key == 0)
+                    {
+                        //override the i value with the string given
+                        if (output[i] == i.ToString())
+                            output[i] = string.Empty;
+
+                        output[i] += args.Value;
+                        //break;
+                    }
+                }
+
+
+            }
+            return output;
+        }
+
+    }
+
+    
+}
